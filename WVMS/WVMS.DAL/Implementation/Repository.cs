72f7705ae067b4f7e4c4ca58b60092cefb92414c
Joshua Eze <ezejoshuac@gmail.com>
2023-03-28 14:18:18 +0100

    Created repository implementation class
    - Implement IRepository
    - Populate IRepository member.

---
+++ WVMS/WVMS.DAL/Implementation/Repository.cs
@@ -0,0 +1,209 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
+using WVMS.DAL.Interfaces;
+
+namespace WVMS.DAL.Implementation
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public T Add(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> AddAsync(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<T> records)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRangeAsync(IEnumerable<T> records)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Any(Expression<Func<T, bool>> predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyAsync(Expression<Func<T, bool>> predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Count(Expression<Func<T, bool>> predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<long> CountAsync(Expression<Func<T, bool>> predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteByIdAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteRange(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteRange(IEnumerable<T> records)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteRangeAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteRangeAsync(IEnumerable<T> records)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, params string[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync(Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetBy(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, int? skip = null, int? take = null, params string[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetByAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, int? skip = null, int? take = null, params string[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetByAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, int? skip = null, int? take = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetQueryable(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, int? skip = null, int? take = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetSingleBy(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetSingleByAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, int? skip = null, int? take = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null, bool tracking = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetSingleByAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> LastAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null, bool disableTracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<decimal> SumAsync(Expression<Func<T, decimal>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SumAsync(Expression<Func<T, int>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<long> SumAsync(Expression<Func<T, long>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRange(IEnumerable<T> records)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateRangeAsync(IEnumerable<T> records)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
