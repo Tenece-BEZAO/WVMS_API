d828904ee761e18753c0ce5019fbf3a08cbeb56e
Joshua Eze <ezejoshuac@gmail.com>
2023-04-06 14:06:57 +0100

    Create admin service controller endpoints
    ## Requirements
    - Lockout users on multiple failed authentications.
    - Admin functionality to lockout specified user for specified timespan.
    - Admin access to user management.

---
+++ WVMS/WVMS.API/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WVMS.BLL.ServicesContract;
+using WVMS.Shared.Dtos.Request;
+
+namespace WVMS.API.Controllers
+{
+    [Route("api/admin")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminService _adminService;
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        [HttpGet]
+        [Route("users")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _adminService.GetAllUsers();
+            if (users == null)
+                return StatusCode(StatusCodes.Status400BadRequest);
+
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Route("users/{id}")]
+        public async Task<IActionResult> GetUser(Guid id)
+        {
+            var user = await _adminService.GetUserById(id);
+            if (user == null)
+                return StatusCode(StatusCodes.Status400BadRequest);
+
+            return Ok(user);
+        }
+
+        [HttpPatch]
+        [Route("users/{id}/lock/")]
+        public async Task<IActionResult> LockUser(Guid id, [FromBody] LockOutDto lockOutDto)
+        {
+            await _adminService.LockUser(id, lockOutDto.Timespan);
+            return Ok("You have been locked out");
+        }
+    }
+}
