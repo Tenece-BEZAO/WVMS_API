f9ac96be6d2cdfea892b710babddf158073d2cba
Joshua Eze <ezejoshuac@gmail.com>
2023-04-09 09:39:56 +0100

    Validate admin creation transfer object

--- WVMS/WVMS.Shared/Dtos/UserForRegistrationDto.cs
+++ WVMS/WVMS.Shared/Dtos/UserForRegistrationDto.cs
@@ -4,16 +4,17 @@ namespace WVMS.Shared.Dtos
 {
     public record UserForRegistrationDto
     {
-        public string? FirstName { get; init; }
-
-        public string? LastName { get; init; }
+        [Required(ErrorMessage = "Firstname is required")]
+        public string FirstName { get; init; }
+        [Required(ErrorMessage = "Lastname is required")]
+        public string LastName { get; init; }
 
         [Required(ErrorMessage = "Username is required")]
-        public string? UserName { get; init; }
+        public string UserName { get; init; }
 
         [Required(ErrorMessage = "Password is required")]
         public string? Password { get; init; }
-
+        [Required(ErrorMessage = "Email is required")]
         public string? Email { get; init; }
 
         public string? PhoneNumber { get; init; }
